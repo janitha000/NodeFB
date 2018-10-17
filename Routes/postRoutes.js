f839e800b5577a6a6b3bda197e7bc599aa5f60a3
Janitha Tennakoon <janitha000@gmail.com>
2018-10-17 12:10:23 +0530

    Added post routes

---
+++ Routes/postRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const Post = require('../Models/post');
+
+router.route('/').get(function(req,res) {
+    Post.find(function(err, posts){
+        if(err)
+            console.log(err)
+
+        res.json(posts);
+    })       
+})
+
+router.route('/').post(function(req,res){
+    post = new Post(req.body);
+    Post.save()
+        .then((post) => {
+            res.status(200).send("Post added");
+
+        })
+        .catch(err => {
+            res.status(400).send("Unable to add post");
+        })
+})

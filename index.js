073e83666cd757a8ab50138dfbdf4d4278f50af0
Janitha Tennakoon <janitha000@gmail.com>
2018-10-17 10:15:43 +0530

    Added client

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-const port = 3000
+
 
 app.use((req,res,next) => {
     console.log(req.headers);
     next();
 })
+
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.get('/', (req,res) =>{
     res.send("Hello Node.js server");
@@ -16,6 +19,8 @@ app.use((err,req,res,next) => {
     console.log(err);
     res.status(500).send('Internal server error');
 })
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, (err) =>{
     if(err)

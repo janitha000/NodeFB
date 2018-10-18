028ca00c2468d26450c5eadcc4383a8fab6cf3f9
Janitha Tennakoon <janitha000@gmail.com>
2018-10-18 09:41:41 +0530

    Added router module

--- Client/NodeFB-client/src/app/app.module.ts
+++ Client/NodeFB-client/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { UiModule } from './ui/ui.module';
+import { RegisterComponent } from './user/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     UiModule
   ],
   providers: [],
